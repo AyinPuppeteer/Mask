7c10dc2fcc13acb6f2de79083ab05d0b6eea7af1
AyinPuppeteer <Ayin.Puppeteer@email.cn>
2026-01-30 23:06:45 +0800

    Update

---
+++ Assets/Scripts/BattleSystem/CamerManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//管理摄像机的脚本
+public class CamerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera MainCamera;
+
+    
+}
